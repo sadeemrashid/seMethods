93b2c0e8c7926be9aa0392d00d0614a79fc3f775
Sabin Constantin Lungu (2B || !2B) <sabinlungu293@gmail.com>
2020-01-13 21:20:00 +0000

    Retrieved Employee data using SQL

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -55,24 +55,25 @@ public class App {
 
     public Employee getEmployee(int ID) {
         try {
-            Statement statement = connection.createStatement();
-
-            String selectString = "SELECT  emp_no, first_name," +
-                    "last_name" + "FROM employees"
-                    + "WHERE emp_no = " + ID;
-
-            ResultSet set = statement.executeQuery(selectString);
-
-            if (set.next()) {
+            // Create an SQL statement
+            Statement stmt = connection.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT emp_no, first_name, last_name "
+                            + "FROM employees "
+                            + "WHERE emp_no = " + ID;
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+            if (rset.next()) {
                 Employee emp = new Employee();
-                emp.emp_no = set.getInt("emp_no");
-                emp.first_name = set.getString("first_name");
-                emp.last_name = set.getString("last_name");
-
+                emp.emp_no = rset.getInt("emp_no");
+                emp.first_name = rset.getString("first_name");
+                emp.last_name = rset.getString("last_name");
                 return emp;
-            } else {
+            } else
                 return null;
-            }
         } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println("Failed to get employee details");
@@ -82,8 +83,14 @@ public class App {
 
     public void displayEmployee(Employee emp) {
         if (emp != null) {
-            System.out.println(emp.emp_no + " "
-                    + emp.first_name);
+            System.out.println(
+                    emp.emp_no + " "
+                            + emp.first_name + " "
+                            + emp.last_name + "\n"
+                            + emp.title + "\n"
+                            + "Salary:" + emp.salary + "\n"
+                            + emp.dept_name + "\n"
+                            + "Manager: " + emp.manager + "\n");
         }
     }
 }
